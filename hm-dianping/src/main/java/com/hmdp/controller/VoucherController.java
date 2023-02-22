5a597c1d96f1c4eac1ec886fb25f398b1ecb723f
zhangz1w3nbeatbox <zhangz1w3nbbx@163.com>
2023-02-22 18:25:48 +0800

    秒杀券的添加功能->(endTime>=currTime)!

--- hm-dianping/src/main/java/com/hmdp/controller/VoucherController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/VoucherController.java
@@ -8,14 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-/**
- * <p>
- *  前端控制器
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+
 @RestController
 @RequestMapping("/voucher")
 public class VoucherController {
--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -2,7 +2,6 @@ package com.hmdp;
 
 import com.hmdp.utils.redisIdWorker;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
