9eb06e99a0f8fb4011e5b7f2f8255b29c58a6f8b
zhangz1w3nbeatbox <zhangz1w3nbbx@163.com>
2023-02-28 15:06:10 +0800

    ✅完成-使用redis的set集合实现共同关注功能

--- hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
@@ -30,7 +30,6 @@ public class FollowController {
     public Result isFollow(@PathVariable("id") Long id){
         return followService.isFollow(id);
     }
-
     //功能: 共同关注
     //问题: 普通方式去db中查找对应关注集合列表 再取交集太耗性能
     //解决: 使用redis的set集合取交集 速度非常快
